3b8fb08b476f41949caeb57392ba4c7dd8ec75d3
hilalsidhic <hilalsidhic21@gmail.com>
2021-10-26 00:54:29 +0530

    sets

--- bin/flutter_basics.dart
+++ bin/flutter_basics.dart
@@ -13,24 +13,29 @@ void main() {
 //   print('Sum = ${inputa + inputb}');
 //   String name = "Hilal Sidhic";
 //   print(name.length);
-  for (int i = 0; i < 10; i++) {
-    print('fd$i');
-  }
-  List<int> numbers = [1, 2, 4, 5, 6, 7, 8];
-  var newst=['string',2,3];
-  var newNumbers = [1,3,4,56,7,8,'hi'];
-  print(numbers.length);
-  print(newNumbers.join('-'));
-  if (newNumbers.contains(3)) {
-    print('3 found');
-  }
-newNumbers.addAll(numbers); 
-newNumbers.add('new variable');
-print(newNumbers);
-newNumbers.removeAt(newNumbers.length-1);
-print(newNumbers);
+//   for (int i = 0; i < 10; i++) {
+//     print('fd$i');
+//   }
+//   List<int> numbers = [1, 2, 4, 5, 6, 7, 8];
+//   var newst=['string',2,3];
+//   var newNumbers = [1,3,4,56,7,8,'hi'];
+//   print(numbers.length);
+//   print(newNumbers.join('-'));
+//   if (newNumbers.contains(3)) {
+//     print('3 found');
+//   }
+// newNumbers.addAll(numbers); 
+// newNumbers.add('new variable');
+// print(newNumbers);
+// newNumbers.removeAt(newNumbers.length-1);
+// print(newNumbers);
 
-List<List<int>> dnums = [[1,2,3],[4,4,6],[9,9,1]];
-var dnums2= [[1,2,'three'],['yes',4,5,6]];
-print(dnums2[0][1]);
+// List<List<int>> dnums = [[1,2,3],[4,4,6],[9,9,1]];
+// var dnums2= [[1,2,'three'],['yes',4,5,6]];
+// print(dnums2[0][1]);
+List<int> numList = [1,2,3,3,4,5,5,5,6,6,6,77,7,7];
+Set<int> numSet = {1,2,3,3,4,5,5,5,6,6,6,77,7,7};
+print(numList);
+print(numSet);
+
 }
